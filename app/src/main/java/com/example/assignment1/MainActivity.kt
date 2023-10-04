008a8a5e5d5c15d5e4f7eb0ad5ca7e3b0fa7a62f
Sterling <sterling1@hotmail.ca>
2023-10-04 19:11:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.assignment1
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
